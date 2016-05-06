8b374bfb4e82e8f3e7a3c7e9fd3120938e6d7e9b
Robert Boggs <robertstephenboggs@gmail.com>
2016-05-06 12:57:55 -0700

    Setup server with babel to compile ES6

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var app = express();
+'use strict';
+
+require('babel-core/register')({});
+require('babel-polyfill');
+
+var server = require('./server').default;
 var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Magic happens on Port", port);
 });
