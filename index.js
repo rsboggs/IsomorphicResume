dde257d11a370f0243a50f40e6b9445dad6d88ec
Robert Boggs <robertstephenboggs@gmail.com>
2016-05-06 18:23:40 -0700

    Render basic HTML

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require('babel-core/register')({});
 require('babel-polyfill');
 
 var server = require('./server').default;
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-server.listen(port, function() {
-  console.log("Magic happens on Port", port);
+server.listen(PORT, function() {
+  console.log("Magic happens on Port", PORT);
 });
--- server.jsx
+++ server.jsx
@@ -1,5 +1,25 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res) => {
+  const HTML = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Isomorphic Resume</title>
+      <script>
+      </script>
+    </head>
+    <body>
+    </body>
+  </html>
+  `;
+
+  res.end(HTML);
+});
 
 export default app;
